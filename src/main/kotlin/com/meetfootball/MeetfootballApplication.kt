f0b570eac6d3819798dd8b1e9ba7478a903ccc7f
MaSungHo <tonem123@naver.com>
2024-06-20 21:06:26 +0900

    :tada: initialize project

---
+++ src/main/kotlin/com/meetfootball/MeetfootballApplication.kt
@@ -0,0 +1,11 @@
+package com.meetfootball
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MeetfootballApplication
+
+fun main(args: Array<String>) {
+	runApplication<MeetfootballApplication>(*args)
+}
---
+++ src/test/kotlin/com/meetfootball/MeetfootballApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.meetfootball
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MeetfootballApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
